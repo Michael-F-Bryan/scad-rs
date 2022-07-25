e5266e1ab48845f062ad2b285489d8f16786336a
Michael-F-Bryan <michaelfbryan@gmail.com>
2022-07-25 13:50:52 +0800

    Created a basic tokenizer

--- crates/scad/src/lib.rs
+++ crates/scad/src/lib.rs
@@ -1,9 +1,19 @@
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod lexer;
 
-    #[test]
-    fn it_works() {
-        assert_eq!(1 + 1, 2);
+use crate::lexer::SyntaxKind;
+use num_traits::FromPrimitive;
+
+#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct Scad;
+
+impl rowan::Language for Scad {
+    type Kind = SyntaxKind;
+
+    fn kind_from_raw(raw: rowan::SyntaxKind) -> Self::Kind {
+        SyntaxKind::from_u16(raw.0).unwrap()
+    }
+
+    fn kind_to_raw(kind: Self::Kind) -> rowan::SyntaxKind {
+        kind.into()
     }
 }
