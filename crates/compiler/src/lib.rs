3f90cf7069a4fabad221e45b62fc1073a6cd5b27
Michael-F-Bryan <michaelfbryan@gmail.com>
2022-08-11 19:23:01 +0800

    Delete the old scad crate

--- crates/compiler/src/lib.rs
+++ crates/compiler/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod diagnostics;
+mod text;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use crate::{
+    diagnostics::{Diagnostic, Diagnostics, IntoDiagnostic, Severity},
+    text::Text,
+};
--- crates/scad/src/lowering/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Lowering a program from its loosely-typed [`crate::ast`] representation to a
-//! strongly-typed [`crate::hir`].
-
-mod query;
-
-pub use self::query::{Lower, LoweringStorage};
